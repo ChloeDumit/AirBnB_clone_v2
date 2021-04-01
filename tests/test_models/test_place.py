4bf135ecf483cc94eb53901aea20344410015135
Lucha3sr <2278@holbertonschool.com>
2021-04-01 05:59:51 +0000

    AirBnB clone - MySQL

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -3,8 +3,13 @@
 
 import unittest
 import os
-from models.place import Place
 from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class TestPlace(unittest.TestCase):
@@ -13,9 +18,19 @@ class TestPlace(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         """set up for test"""
+        cls.state = State()
+        cls.state.name = "California"
+        cls.city = City()
+        cls.city.name = "San francisco"
+        cls.city.state_id = "CA"
+        cls.user = User()
+        cls.user.first_name = "FN"
+        cls.user.last_name = "LN"
+        cls.user.email = "my@me"
+        cls.user.password = "pwd"
         cls.place = Place()
-        cls.place.city_id = "1234-abcd"
-        cls.place.user_id = "4321-dcba"
+        cls.place.city_id = "new city ID"
+        cls.place.user_id = "new user ID"
         cls.place.name = "My_house"
         cls.place.description = "no_description_yet"
         cls.place.number_rooms = 4
@@ -29,6 +44,22 @@ class TestPlace(unittest.TestCase):
 
     def test_attributes_Place(self):
         """tests if amenity have attributes"""
+        self.assertTrue('id' in self.state.__dict__)
+        self.assertTrue('created_at' in self.state.__dict__)
+        self.assertTrue('updated_at' in self.state.__dict__)
+        self.assertTrue('name' in self.state.__dict__)
+        self.assertTrue('id' in self.city.__dict__)
+        self.assertTrue('created_at' in self.city.__dict__)
+        self.assertTrue('updated_at' in self.city.__dict__)
+        self.assertTrue('state_id' in self.city.__dict__)
+        self.assertTrue('name' in self.city.__dict__)
+        self.assertTrue('email' in self.user.__dict__)
+        self.assertTrue('id' in self.user.__dict__)
+        self.assertTrue('created_at' in self.user.__dict__)
+        self.assertTrue('updated_at' in self.user.__dict__)
+        self.assertTrue('password' in self.user.__dict__)
+        self.assertTrue('first_name' in self.user.__dict__)
+        self.assertTrue('last_name' in self.user.__dict__)
         self.assertTrue('id' in self.place.__dict__)
         self.assertTrue('created_at' in self.place.__dict__)
         self.assertTrue('updated_at' in self.place.__dict__)
@@ -44,8 +75,21 @@ class TestPlace(unittest.TestCase):
         self.assertTrue('longitude' in self.place.__dict__)
         self.assertTrue('amenity_ids' in self.place.__dict__)
 
-    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db',
-                     "This test only work in Filestorage")
+    def test_save_State(self):
+        """test if save works"""
+        self.state.save()
+        self.assertNotEqual(self.state.created_at, self.state.updated_at)
+
+    def test_save_City(self):
+        """test if save works"""
+        self.city.save()
+        self.assertNotEqual(self.city.created_at, self.city.updated_at)
+
+    def test_save_User(self):
+        """test if save works"""
+        self.user.save()
+        self.assertNotEqual(self.user.created_at, self.user.updated_at)
+
     def test_save_Place(self):
         """test if save works"""
         self.place.save()
