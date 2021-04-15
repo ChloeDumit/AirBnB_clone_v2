3f0720e9e46ee48c7d1b18ca7e546fa056d88f7c
ChloeDumit <dumitchloe@gmail.com>
2021-04-15 15:38:01 +0000

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -16,4 +16,3 @@ ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i "42i\ \n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 service nginx restart
-exit 0
