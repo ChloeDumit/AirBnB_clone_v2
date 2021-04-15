3962296b844339c3b08cd79ee4b565adb09c4023
ChloeDumit <dumitchloe@gmail.com>
2021-04-15 13:13:45 +0000

    tasks

---
+++ 1-pack_web_static.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" create a .tgz file"""
+from datetime import datetime
+from fabric.api import *
+def do_pack():
+    """ create a .tgz file"""
+    today = datetime.now().strftime('%Y%m%d%H%M%S')
+    local("mkdir -p versions/")
+    try:
+        local("tar -cvzf versions/web_static_{}.tgz web_static".format(today))
+        return "versions/web_static/{}".format(today)
+    except Exception:
+        return None
