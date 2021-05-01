1bb7f17a4e6d6e8d1598b36884638e9f236cff36
ChloeDumit <2106@holbertonschool.com>
2021-05-01 20:58:58 +0000

    task 7

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -66,3 +66,8 @@ class FileStorage:
         if k in self.__objects:
             del self.__objects[k]
             self.save()
+
+ def close(self):
+        """ This method calls the reload method
+        """
+        self.reload()
